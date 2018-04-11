c52e9fc52bc82d1b842023f2a02f3fdd0bd296bf
ana.rajner@gmail.com <ana.rajner@gmail.com>
2018-04-11 09:02:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function doToElementsInArray(array,callback){
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array){
   array[index] = 'and now ${array[index]}';
-  
+
 }
